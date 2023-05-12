e8d1da8b878a3e6ae261d00ed1d5eed80b6975cb
OlgerOrtiz <olgerortiz24@gmail.com>
2023-05-12 13:48:09 -0400

    styles components

--- src/components/Purchases/ProductPurchase.jsx
+++ src/components/Purchases/ProductPurchase.jsx
@@ -2,21 +2,20 @@ import React from 'react'
 import './styles/productPurchase.css'
 
 export const ProductPurchase = ({ prodPurchased }) => {
+  console.log(prodPurchased);
 
   return (
-    <div className='purchases__content'>
-      <article className='purchases__container'>
-        <div className='purchases__imgContainer'>
-          <img className='purchases__img' src={prodPurchased.product.images[0].url} alt="" />
+    <article className='productPurchase__container'>
+      <div className='productPurchase__content'>
+        <div className='productPurchase__imgContainer'>
+          <img className='productPurchase__img' src={prodPurchased.product.images[2].url} alt="" />
         </div>
-        <h3 className='purchases__productName'>{prodPurchased.product.title}</h3>
-        <div className='purchases__data'>
-          <span className='purchases__quantity'>Quantity: <span className='purchases__value'>{prodPurchased.quantity}</span>
-          </span>
-          <span className='purchases__subtotal'>Subtotal: <span className='purchases__value'>${prodPurchased.quantity * prodPurchased.product.price}.00</span>
-          </span>
+        <h3 className='productPurchase__productName'>{prodPurchased.product.title}</h3>
+        <div className='productPurchase__data'>
+          <h3 className='productPurchase__quantity'>{prodPurchased.quantity}</h3>
+          <h3 className='productPurchase__price'>$ {prodPurchased.quantity * prodPurchased.product.price}</h3>
         </div>
-      </article>
-    </div>
+      </div>
+    </article>
   )
 }
