2ddc3f1ebe049e793bbb0a98978d66279513a9f8
ZaraisGOLD <zaraisgold@gmail.com>
2023-05-12 06:02:06 -0400

    css purchases

--- src/components/Purchases/ProductPurchase.jsx
+++ src/components/Purchases/ProductPurchase.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
+import './styles/productPurchase.css'
 
 export const ProductPurchase = ({ prodPurchased }) => {
 
-
   return (
-    <article>
-      <img src={prodPurchased.product.images[0].url} alt="" />
-      <h3>{prodPurchased.product.title}</h3>
-      <span>{prodPurchased.quantity}</span>
-      <span>{prodPurchased.quantity * prodPurchased.product.price}</span>
-    </article>
+    <div className='purchases__content'>
+      <article className='purchases__container'>
+        <div className='purchases__imgContainer'>
+          <img className='purchases__img' src={prodPurchased.product.images[0].url} alt="" />
+        </div>
+        <h3 className='purchases__productName'>{prodPurchased.product.title}</h3>
+        <div className='purchases__data'>
+          <span className='purchases__quantity'>Quantity: <span className='purchases__value'>{prodPurchased.quantity}</span>
+          </span>
+          <span className='purchases__subtotal'>Subtotal: <span className='purchases__value'>${prodPurchased.quantity * prodPurchased.product.price}.00</span>
+          </span>
+        </div>
+      </article>
+    </div>
   )
 }
--- src/pages/Purchases.jsx
+++ src/pages/Purchases.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
 import usePurchases from '../hooks/usePurchases'
 import { ProductPurchase } from '../components/Purchases/ProductPurchase'
+import './styles/purchases.css'
 
 const Purchases = () => {
 
@@ -10,10 +11,9 @@ const Purchases = () => {
     getPurchased()
   }, [])
 
-
   return (
-    <div>
-      <h2>Purchases</h2>
+    <div className='purchases'>
+      <h2 className='purchases__title'>Purchases</h2>
       {
         purchases?.map(prodPurchased => (
           <ProductPurchase
