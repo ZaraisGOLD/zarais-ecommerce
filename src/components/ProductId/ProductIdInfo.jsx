cdb8b16c345f3132b5d9977a29d0722d8a572beb
OlgerOrtiz <olgerortiz24@gmail.com>
2023-05-12 08:21:02 -0400

    styles components

--- src/components/ProductId/ProductIdInfo.jsx
+++ src/components/ProductId/ProductIdInfo.jsx
@@ -25,27 +25,37 @@ const ProductIdInfo = ({ product }) => {
   }
 
   return (
-    <section className='productIdInfo__content'>
-      <h3>{product?.brand}</h3>
-      <h2>{product?.title}</h2>
-      <p>{product?.description}</p>
-      <footer>
-        <div>
-          <span>Price</span>
-          <span>${product?.price}.00</span>
-        </div>
-        <div>
-          <span>Quantity</span>
-          <div>
-            <button onClick={handleMinus}>-</button>
-            <button>{quantity}</button>
-            <button onClick={handlePlus}>+</button>
+    <section className='productIdInfo__container'>
+      <section className='productIdInfo__name'>
+        <span className='productIdInfo__name--brand'>{product?.brand}</span>
+        <h3 className='productIdInfo__name--title'>{product?.title}</h3>
+      </section>
+      <section className='productIdInfo__body--container'>
+        <section className='productIdInfo__body--content'>
+          <div className='productIdInfo__priceAndQuantity--container'>
+            <div className='productIdInfo__price'>
+              <span className='price__label'>Price</span>
+              <h4 className='price__value'>$ {product?.price}</h4>
+            </div>
+            <div className='productIdInfo__quantity'>
+              <span className='quantity__label'>Quantity</span>
+              <div className='quantity__counter'>
+                <button className='counter__btn--minus' onClick={handleMinus}>
+                <i className='bx bx-minus'></i>
+                </button>
+                <button className='counter__value'>{quantity}</button>
+                <button className='counter__btn--plus' onClick={handlePlus}>
+                <i className='bx bx-plus'></i>
+                </button>
+              </div>
+            </div>
           </div>
-        </div>
-        <button>
-          <i onClick={handleAddToCart} className='bx bx-cart'></i>
-        </button>
-      </footer>
+          <button className='productIdInfo__btn--addToCart'>
+            Add to cart <i onClick={handleAddToCart} className='bx bx-cart btn__icon--cart'></i>
+          </button>
+        </section>
+        <p className='productIdinfo__body--description'>{product?.description}</p>
+      </section>
     </section>
   )
 }
